3d9aebff1be86081adaaf253d14cfe98897de57b
Cui Liqiang <cui.liqiang@gmail.com>
2015-10-13 19:01:26 +0800

    remove some useless code

--- app/githubApiSample/SampleController.scala
+++ app/githubApiSample/SampleController.scala
@@ -1,12 +1,10 @@
 package githubApiSample
 
-import com.qifun.jsonStream.rpc.ICompleteHandler1
 import com.thoughtworks.restRpc.play.Implicits._
-import model.User
 import play.api.mvc._
 import rpc.IUserRpc
 
-import scala.concurrent.{Promise, ExecutionContext, Future}
+import scala.concurrent.{Promise, ExecutionContext}
 import scala.util.{Failure, Success}
 
 class SampleController(userRpc: IUserRpc)(implicit ec: ExecutionContext) extends Controller {
